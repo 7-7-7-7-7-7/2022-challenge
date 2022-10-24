b7682eaa583ec0be911af841e68e8689efac5b44
Rodolfo Thiede <hello@rodolfothiede.me>
2022-10-24 10:27:59 -0300

    Adding update method

--- api/app/controllers/posts_controller.rb
+++ api/app/controllers/posts_controller.rb
@@ -17,9 +17,24 @@ class PostsController < ApplicationController
     render json: @post
   end
 
+  def update
+    @post = Post.find(params[:id])
+    @post.update(
+      name: params[:name],
+      description: params[:description]
+    )
+    render json: @post
+  end
+
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
     render json: @post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :description)
   end
 end
